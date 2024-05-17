da3d50e74e2a8627e4c01fe3fab703ee90b85320
BlameTheNewGuy17 <101384843+BlameTheNewGuy17@users.noreply.github.com>
2024-05-16 18:58:38 -0700

    Initial commit

---
+++ Source/TrainGameUE54/TrainGameUE54.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TrainGameUE54.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TrainGameUE54, "TrainGameUE54" );
